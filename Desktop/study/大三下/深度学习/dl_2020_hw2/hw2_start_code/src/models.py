55704830b57d1d41f4a734ea1e04e2e86f0c845f
Holycomfort <46879410+Holycomfort@users.noreply.github.com>
2020-04-15 23:52:44 +0800

    up

--- Desktop/study/大三下/深度学习/dl_2020_hw2/hw2_start_code/src/models.py
+++ Desktop/study/大三下/深度学习/dl_2020_hw2/hw2_start_code/src/models.py
@@ -9,8 +9,10 @@ def model_A(num_classes, pretrained=True):
     return model_resnet
 
 def model_B(num_classes, pretrained = False):
-    ## your code here
-    pass
+    model_resnet = models.resnet18(pretrained=pretrained)
+    num_features = model_resnet.fc.in_features
+    model_resnet.fc = nn.Linear(num_features, num_classes)
+    return model_resnet
 
 
 def model_C(num_classes, pretrained = False):
